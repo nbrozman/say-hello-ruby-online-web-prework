fe643b4d8773880c4fd7e808f6943fcd8230cfc0
Nick Brozman <nick.brozman@gmail.com>
2019-05-26 15:38:22 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -9,6 +9,7 @@ describe "say_hello" do
 
   it "defaults to Ruby Programmer when no name is passed in" do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+  end
     say_hello()
   end
 end
